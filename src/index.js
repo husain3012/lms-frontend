3a9ffab1327685f64f6c92c603d49f6e175c324e
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-05-12 18:52:23 +0530

    faltu ka code hatadiya

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import axios from "axios";
 import * as serviceWorker from "./serviceWorker";
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers.common["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept";
-axios.defaults.headers.common["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, PATCH, OPTIONS";
-axios.defaults.headers.common["Access-Control-Allow-Credentials"] = "true";
 
 ReactDOM.render(
   <React.StrictMode>
