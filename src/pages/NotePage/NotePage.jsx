5ca5121095ade9d1f49d1e3e20e787e9aeef4c41
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-05-14 14:30:39 +0530

    major UI Fix

--- src/pages/NotePage/NotePage.jsx
+++ src/pages/NotePage/NotePage.jsx
@@ -156,8 +156,10 @@ const NotePage = () => {
     } else {
       makeToast.warning("Invalid URL");
     }
+    
   };
 
+  console.log(allSubmissions);
   return (
     <>
       <Navbar right={<NavUser />} />
@@ -187,105 +189,8 @@ const NotePage = () => {
                 <div className={classes.noteContent}>
                   <Interweave content={note.body} />
                 </div>
-              </div>
-            </div>
-            <div className={classes.rightPane}>
-              {note.type === "assignment" && userType === "student" && (
-                <>
-                  <div className={classes.submission}>
-                    <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
-                      Your Work
-                    </h3>
-                    <FileUpload setAllowUpload={setAllowSubmit} setUploadedFile={setSubmissionFile} uploadedFile={submissionFile} />
-                    <h3 style={{ fontSize: "0.9rem", textAlign: "center" }}>OR</h3>
-                    <input className={classes.submissionInput} placeholder="Enter work URL" value={submissionUrl} onChange={(e) => setSubmissionUrl(e.target.value)} />
-                    {allowSubmit && (
-                      <StyledButton color={themeColor} onClick={assignmentSubmitHandler}>
-                        Submit
-                      </StyledButton>
-                    )}
-                  </div>
 
-                  {submissionHistory.length > 0 && (
-                    <div className={classes.submissionHistory}>
-                      <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
-                        Submission History
-                      </h3>
-                      <div className={classes.submissionHistoryList}>
-                        {submissionHistory.map((submission) => (
-                          <div className={classes.submissionHistoryItem}>
-                            <p>
-                              <AiOutlineHistory /> {dayjs(submission.created_at).format("YYYY-MM-DD, hh:mm A")}{" "}
-                            </p>
-                            <div className={classes.historyItemButtons}>
-                              <a href={submission.url} target="_blank" rel="noopener noreferrer" download="file">
-                                <AiOutlineDownload />
-                              </a>
-                              <button>
-                                <FiRepeat />
-                              </button>
-                            </div>
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                </>
-              )}
-              {note.type === "assignment" && userType === "teacher" && (
-                <>
-                  <div className={classes.allSubmissions}>
-                    <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
-                      All submissions
-                    </h3>
-
-                    <Accordion>
-                      {allSubmissions
-                        .filter((sub) => sub.submissions.length > 0)
-                        .map((submission) => (
-                          <Accordion.Item key={submission.student_id}>
-                            <Accordion.Header className={classes.accordionHeader}>{submission.student_name}</Accordion.Header>
-                            <Accordion.Body>
-                              {submission.submissions.map((sub) => (
-                                <div className={classes.allSubmissionItem}>
-                                  <span>
-                                    <AiOutlineHistory /> {dayjs(sub.created_at).format("YYYY-MM-DD, hh:mm A")}
-                                  </span>
-                                  <div className={classes.submissionItemButtons}>
-                                    <a href={sub.url} target="_blank" rel="noopener noreferrer" download="file">
-                                      <AiOutlineDownload />
-                                    </a>
-                                  </div>
-                                </div>
-                              ))}
-                            </Accordion.Body>
-                          </Accordion.Item>
-                        ))}
-                    </Accordion>
-                  </div>
-                  {allSubmissions.some((sub) => sub.submissions.length === 0) && (
-                    <div className={classes.allSubmissions}>
-                      <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
-                        Missing submissions
-                      </h3>
-
-                      {allSubmissions
-                        .filter((sub) => sub.submissions.length === 0)
-                        .map((submission) => (
-                          <div key={submission.student_id} className={classes.missingSubmissionItem}>
-                            <span>{submission.student_name}</span>
-                            <a href={`mailto:${submission.student_email}`}>
-                              <AiOutlineMail />
-                            </a>
-                          </div>
-                        ))}
-                    </div>
-                  )}
-                </>
-              )}
-            </div>
-          </div>
-          {note.type === "assignment" && userType === "teacher" && (
+                {note.type === "assignment" && userType === "teacher" && (
             <div className={classes.submissionStats}>
               <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
                 Submission Stats
@@ -347,6 +252,106 @@ const NotePage = () => {
               </div>
             </div>
           )}
+              </div>
+              
+            </div>
+            <div className={classes.rightPane}>
+              {note.type === "assignment" && userType === "student" && (
+                <>
+                  <div className={classes.submission}>
+                    <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
+                      Your Work
+                    </h3>
+                    <FileUpload setAllowUpload={setAllowSubmit} setUploadedFile={setSubmissionFile} uploadedFile={submissionFile} />
+                    <h3 style={{ fontSize: "0.9rem", textAlign: "center" }}>OR</h3>
+                    <input className={classes.submissionInput} placeholder="Enter work URL" value={submissionUrl} onChange={(e) => setSubmissionUrl(e.target.value)} />
+                    {allowSubmit && (
+                      <StyledButton color={themeColor} onClick={assignmentSubmitHandler}>
+                        Submit
+                      </StyledButton>
+                    )}
+                  </div>
+
+                  {submissionHistory.length > 0 && (
+                    <div className={classes.submissionHistory}>
+                      <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
+                        Submission History
+                      </h3>
+                      <div className={classes.submissionHistoryList}>
+                        {submissionHistory.map((submission) => (
+                          <div className={classes.submissionHistoryItem}>
+                            <p>
+                              <AiOutlineHistory /> {dayjs(submission.created_at).format("YYYY-MM-DD, hh:mm A")}{" "}
+                            </p>
+                            <div className={classes.historyItemButtons}>
+                              <a href={submission.url} target="_blank" rel="noopener noreferrer" download="file">
+                                <AiOutlineDownload />
+                              </a>
+                              <button>
+                                <FiRepeat />
+                              </button>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </>
+              )}
+              {note.type === "assignment" && userType === "teacher" && (
+                <>
+                  <div className={classes.allSubmissions}>
+                    <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
+                      All submissions
+                    </h3>
+
+                    <Accordion   >
+                      {allSubmissions
+                        .filter((sub) => sub.submissions.length > 0)
+                        .map((submission) => (
+                          <Accordion.Item  key={submission.student_id}>
+                            <Accordion.Header className={classes.accordionHeader}>{submission.student_name}</Accordion.Header>
+                            <Accordion.Body>
+                              {submission.submissions.map((sub) => (
+                                <div className={classes.allSubmissionItem}>
+                                  <span>
+                                    <AiOutlineHistory /> {dayjs(sub.created_at).format("YYYY-MM-DD, hh:mm A")}
+                                  </span>
+                                  <div className={classes.submissionItemButtons}>
+                                    <a href={sub.url} target="_blank" rel="noopener noreferrer" download="file">
+                                      <AiOutlineDownload />
+                                    </a>
+                                  </div>
+                                </div>
+                              ))}
+                            </Accordion.Body>
+                          </Accordion.Item>
+                        ))}
+                    </Accordion>
+                  </div>
+                  {allSubmissions.some((sub) => sub.submissions.length === 0) && (
+                    <div className={classes.allSubmissions}>
+                      <h3 className={classes.submissionTitle} style={{ color: themeColor }}>
+                        Missing submissions
+                      </h3>
+
+                      {allSubmissions
+                        .filter((sub) => sub.submissions.length === 0)
+                        .map((submission) => (
+                          <div key={submission.student_id} className={classes.missingSubmissionItem}>
+                            <span>{submission.student_name}</span>
+                            <a href={`mailto:${submission.student_email}`}>
+                              <AiOutlineMail />
+                            </a>
+                          </div>
+                        ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+          </div>
+         
         </div>
       )}
     </>
